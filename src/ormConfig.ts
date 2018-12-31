87001e6d3bbb30d7f79997c75cfd783c419fd874
Uco <mmdsds@u.sogang.ac.kr>
2018-12-31 01:56:48 -0500

    qraphql-typescript merge setting

---
+++ src/ormConfig.ts
@@ -0,0 +1,16 @@
+import { ConnectionOptions } from "typeorm";
+
+const connectionOptions : ConnectionOptions = {
+    type: "postgres",
+    database : "Uber",
+    synchronize: true,
+    logging: true,
+    entities: ["entities/**/*.*"],
+    host: process.env.DB_ENDPOINT || "localhost",
+    port: 5432,
+    username: process.env.DB_USERNAME || "moondaesung",
+    password: process.env.DB_PASSWORD || "",
+    
+};
+
+export default connectionOptions;
